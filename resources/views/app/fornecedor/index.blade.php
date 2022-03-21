803a0a40056469bc7202eb4f54078f2621cbc4f6
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-03-21 16:45:53 -0300

    Blade - Incluíndo comentários e blocos PHP puros

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,11 @@
+<h3>Fornecedor</h3>
+
+{{'Texto de teste'}}
+
+<?= 'Texto de teste' ?>
+
+{{-- Aqui fica o comentário que será descartado pelo interpretador do blade --}}
+
+@php
+    // Usando a sintax nativa do php para fazer os comentários
+@endphp
