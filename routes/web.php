cbc4d4967f82de8d36ad067f4e58842417a05d86
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-03-21 14:05:21 -0300

    Definindo novas rotas: Login, clientes, fornecedores e Produtos

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,14 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 
+Route::get('/login', function (){ return 'Login'; });
+
+Route::get('/clientes', function (){ return 'Clientes'; });
+
+Route::get('/fornecedores', function (){ return 'Fornecedores'; });
+
+Route::get('/produtos', function (){ return 'Produtos'; });
+
 /*
 Rota com parâmetro
 
