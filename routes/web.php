726255682d9d2b43e501b3b5a4252129c0e92df6
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-03-21 13:57:33 -0300

    Criando um menu de navegação

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,17 @@ Route::get('/', function () {
     return 'Olá, seja bem vindo ao curso';
 });
 */
+/* verbo http
 
+get
+post
+put
+patch
+delete
+options
+
+*/
+// Route::get($uri, $callback);
 Route::get('/', 'PrincipalController@principal');
 
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
@@ -37,24 +47,13 @@ e definir um valor default para a variável em questão
 
 Os parâmetros opcionais deves ser definidos da direita para a esquerda sempre, caso contrário o framework ficará perdido
 */
-Route::get('/contato/{nome}/{categoria_id}',
-    function (
-        string $nome = 'Desconhecido',
-        int $categoria_id = 1 // 1 - Informação
-    ) {
-        echo 'Rota contato com parâmetros <br>Nome: ' . $nome . ' <br>Categoria: ' . $categoria_id;
-    }
-) -> where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+//Route::get('/contato/{nome}/{categoria_id}',
+//    function (
+//        string $nome = 'Desconhecido',
+//        int $categoria_id = 1 // 1 - Informação
+//    ) {
+//        echo 'Rota contato com parâmetros <br>Nome: ' . $nome . ' <br>Categoria: ' . $categoria_id;
+//    }
+//) -> where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
 
-// Route::get($uri, $callback);
 
-/* verbo http
-
-get
-post
-put
-patch
-delete
-options
-
-*/
