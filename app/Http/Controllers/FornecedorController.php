c10dd7549620ff06105cdc5c771fa82537f4d538
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-03-22 13:40:44 -0300

    Operador codicional de valor default ??

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -14,15 +14,13 @@ class FornecedorController extends Controller
             0 => [
                 'nome' => 'Fornecedor 1',
                 'status' => 'N',
-                'cnpj' => '00.000.000/000-00'
+                'cnpj' => '0.000.000/000-00'
             ],
             1 => [
                 'nome' => 'Fornecedor 2',
                 'status' => 'N'
             ]
         ];
-
-        echo isset($fornecedores[1]['cnpj']) ? 'CNPJ informado' : 'CNPJ não informado';
 
         return view('app.fornecedor.index', compact('fornecedores'));
     }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -19,10 +19,10 @@
     <br>
     Status: {{$fornecedores[0]['status']}}
     <br>
-    @isset($fornecedores[0]['cnpj'])
-        CNPJ: {{$fornecedores[0]['cnpj']}}
-        @empty($fornecedores[0]['cnpj'])
-            - Vazio
-        @endempty
-    @endisset
+    CNPJ: {{$fornecedores[1]['cnpj'] ?? 'Dado não foi preenchido'}}
+    <!--
+        $variavel testada não estiver definida (isset)
+        ou
+        $variavel testada possui valor null
+    -->
 @endisset
