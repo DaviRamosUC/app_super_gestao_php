36a6c0328eaecdd0b4399ec8c0b09abb71c0bc9c
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-03-28 15:03:59 -0300

    Entendendo o objeto Request

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request $request)
     {
+        echo '<pre>';
+        print_r($request->all());
+        echo '</pre>';
+        echo $request->input('nome');
+        echo '<br>';
+        echo $request->input('email');
         return view('site.contato');
     }
 }
