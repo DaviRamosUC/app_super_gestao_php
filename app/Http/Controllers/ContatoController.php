e3130a1f73b3ac477f43297b5b26606da12e345c
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-03-28 15:22:32 -0300

    Gravando dados do formulário no banco de dados

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,17 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request)
     {
-        echo '<pre>';
+        /*echo '<pre>';
         print_r($request->all());
         echo '</pre>';
         echo $request->input('nome');
         echo '<br>';
-        echo $request->input('email');
+        echo $request->input('email');*/
+
+        /*
+         * Aqui é possível instanciar o objeto com os attr do objeto request
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+//        print_r($contato->getAttributes());
+        $contato->save();
+        */
+
+        //Este código faz o mesmo que o de cima
+//        $contato = new SiteContato();
+//        $contato->create($request->all());
+//        print_r($contato->getAttributes());
+//        $contato->save();
+
         return view('site.contato');
     }
 }
