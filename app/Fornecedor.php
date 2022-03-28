aaaf483e10fb4c49de849498154c70b3fb3cb737
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-03-28 10:30:32 -0300

    Eloquent - Implementando o recurso de softDelete

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -3,11 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-//fornecedors
-
+// nome padrão para a tabela no db = fornecedors
 class Fornecedor extends Model
 {
+    //Implementando o SoftDeletes
+    use SoftDeletes;
+
+    // Nome alterado para correção no db
     protected $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
