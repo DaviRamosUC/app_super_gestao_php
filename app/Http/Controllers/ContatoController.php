bc2f3f2bed2e9e617037a13a674173da2f7ecf8e
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-03-28 16:29:15 -0300

    Repopulando o formulário (Request Old Input) com selected

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,9 +7,15 @@ use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request $request)
     {
-        return view('site.contato');
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação',
+        ];
+
+        return view('site.contato', ['motivo_contatos' => $motivo_contatos]);
     }
 
     public function salvar(Request $request)
