b290d36deee09b1b1e67e3d74d7fdb477daf096b
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-03-29 17:04:50 -0300

    Implementando middlewares no método construtor dos controllers
    Signed-off-by: Davi Ramos Lima <davi.lima@ucsal.edu.br>

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\LogAcessoMiddleware;
 use Illuminate\Http\Request;
 
 class SobreNosController extends Controller
 {
-    public function sobreNos(){
+    //Adicionando o middleware nom momento da instância do controller
+    public function __construct()
+    {
+        $this->middleware(LogAcessoMiddleware::class);
+    }
+
+    public function sobreNos()
+    {
         return view('site.sobre-nos');
     }
 }
