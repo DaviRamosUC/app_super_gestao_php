824d13ecc5e8adcbd29595b6d6c93e2fa6f3f6ed
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-03-29 17:01:29 -0300

    Criando o model de LogAcesso.php e migration, adicionando método create do log no middleware LogAcessoMiddleware.php
    Signed-off-by: Davi Ramos Lima <davi.lima@ucsal.edu.br>

--- app/Http/Middleware/LogAcessoMiddleware.php
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\LogAcesso;
 use Closure;
 
 class LogAcessoMiddleware
@@ -9,15 +10,15 @@ class LogAcessoMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        //$request - manipular
-//        return $next($request);
-        //response - manipular
+        $ip = $request->server->get('REMOTE_ADDR');
+        $rota = $request->getRequestUri();
+        LogAcesso::create(['log' => "IP $ip requisitou a rota $rota"]);
         return Response('Chegamos ao middleware e finalizamos no próprio método');
     }
 }
