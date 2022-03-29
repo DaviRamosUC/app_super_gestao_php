9770eb11fe39e3af531bef18c564f8d1b4419516
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-03-29 17:16:04 -0300

    Implementando middlewares em todas requesições do tipo web no Kernel.php
    Signed-off-by: Davi Ramos Lima <davi.lima@ucsal.edu.br>

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -2,16 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Middleware\LogAcessoMiddleware;
-use Illuminate\Http\Request;
-
 class SobreNosController extends Controller
 {
-    //Adicionando o middleware nom momento da instância do controller
-    public function __construct()
-    {
-        $this->middleware(LogAcessoMiddleware::class);
-    }
+//    //Adicionando o middleware nom momento da instância do controller
+//    public function __construct()
+//    {
+//        $this->middleware(LogAcessoMiddleware::class);
+//    }
 
     public function sobreNos()
     {
--- routes/web.php
+++ routes/web.php
@@ -29,15 +29,11 @@ options
 
 */
 // Route::get($uri, $callback);
-Route::get('/', 'PrincipalController@principal')
-    ->name("site.index")
-    ->middleware(LogAcessoMiddleware::class);
+Route::get('/', 'PrincipalController@principal')->name("site.index");
 
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name("site.sobrenos");
 
-Route::get('/contato', 'ContatoController@contato')
-    ->name("site.contato")
-    ->middleware(LogAcessoMiddleware::class);
+Route::get('/contato', 'ContatoController@contato')->name("site.contato");
 
 Route::get('/login', function () {
     return 'Login';
