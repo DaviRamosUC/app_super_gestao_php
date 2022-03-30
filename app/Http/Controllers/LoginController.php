1ac60297062178e40b3887a99bdafb0b0b38668c
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-03-30 12:19:17 -0300

    Recebendo os parâmetros de usuário e senha
    Signed-off-by: Davi Ramos Lima <davi.lima@ucsal.edu.br>

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class LoginController extends Controller
 {
     public function index()
@@ -9,8 +11,23 @@ class LoginController extends Controller
         return view('site.login', ['titulo' => 'Login']);
     }
 
-    public function autenticar()
+    public function autenticar(Request $request)
     {
-        return 'Chegamos até aqui!!';
+        //regras de validação
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required',
+        ];
+        //as mensagens de feedback de validação
+        $feedback = [
+            'usuario.email' => 'O email informado não é válido',
+            'required' => 'O campo senha é obrigatório',
+        ];
+
+        //se não passar pelo validate
+        //caso não passe pela validação ele voltará para a página anterior
+        $request->validate($regras, $feedback);
+
+        print_r($request->all());
     }
 }
