8df18dd2ba066091e46ccd8ca362da085571c100
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-03-30 14:41:24 -0300

    Redirect com envio de parâmetros - Apresentando mensagem de erro de login
    Signed-off-by: Davi Ramos Lima <davi.lima@ucsal.edu.br>

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,9 +7,13 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('site.login', ['titulo' => 'Login']);
+        $erro = '';
+        if ($request->get('erro')==1){
+            $erro = 'Usuário e/ou senha não existe';
+        }
+        return view('site.login', ['titulo' => 'Login', 'erro' => $erro]);
     }
 
     public function autenticar(Request $request)
@@ -42,7 +46,7 @@ class LoginController extends Controller
         if (isset($usuario->name)) {
             echo 'Usuário existe';
         } else {
-            echo 'Usuário não existe';
+            return redirect()->route('site.login', ['erro' => 1]);
         }
 
     }
