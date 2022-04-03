c1af4d8d4b8d405838d07bca079444c3bb99dd78
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-04-03 17:49:16 -0300

    Implementando o cadastro de produtos parte(delete)

--- resources/views/app/produto/create.blade.php
+++ resources/views/app/produto/create.blade.php
@@ -11,37 +11,34 @@
 
         <div class="menu">
             <ul>
-                <li><a href="{{route('produto.index')}}">Voltar</a></li>
+                <li><a href="{{ route('produto.index') }}">Voltar</a></li>
             </ul>
         </div>
 
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                <form action="{{route('produto.store')}}" method="post">
+                <form action="{{ route('produto.store') }}" method="post">
                     @csrf
-                    <input type="text" name="nome" value="{{old('nome')}}" placeholder="Nome"
-                           class="borda-preta">
-                    {{$errors->has('nome') ? $errors->first('nome'): ''}}
+                    <input type="text" name="nome" value="{{ old('nome') }}" placeholder="Nome" class="borda-preta">
+                    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
 
-                    <input type="text" name="descricao" value="{{old('descricao')}}" placeholder="Descrição"
-                           class="borda-preta">
-                    {{$errors->has('descricao') ? $errors->first('descricao'): ''}}
+                    <input type="text" name="descricao" value="{{ old('descricao') }}" placeholder="Descrição"
+                        class="borda-preta">
+                    {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
 
-                    <input type="text" name="peso" value="{{old('peso')}}" placeholder="Peso"
-                           class="borda-preta">
-                    {{$errors->has('peso') ? $errors->first('peso'): ''}}
+                    <input type="text" name="peso" value="{{ old('peso') }}" placeholder="Peso" class="borda-preta">
+                    {{ $errors->has('peso') ? $errors->first('peso') : '' }}
 
                     <select name="unidade_id">
                         <option value="">-- Selecione a Unidade de Medida --</option>
-                        @foreach($unidades as $unidade)
-                            <option
-                                value="{{$unidade->id}}" {{old('unidade_id'==$unidade->id ? 'selected' : '')}}>{{$unidade->descricao}}</option>
+                        @foreach ($unidades as $unidade)
+                            <option value="{{ $unidade->id }}" {{ old('unidade_id' == $unidade->id ? 'selected' : '') }}>
+                                {{ $unidade->descricao }}</option>
                         @endforeach
                     </select>
-                    {{$errors->has('unidade_id') ? $errors->first('unidade_id'): ''}}
+                    {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : '' }}
 
-                    <button type="submit"
-                            class="borda-preta">'Cadastrar'}}</button>
+                    <button type="submit" class="borda-preta">Cadastrar</button>
                 </form>
             </div>
         </div>
--- resources/views/app/produto/index.blade.php
+++ resources/views/app/produto/index.blade.php
@@ -11,7 +11,7 @@
 
         <div class="menu">
             <ul>
-                <li><a href="{{route('produto.create')}}">Novo</a></li>
+                <li><a href="{{ route('produto.create') }}">Novo</a></li>
             </ul>
         </div>
 
@@ -19,34 +19,44 @@
             <div style="width: 90%; margin-left: auto; margin-right: auto;">
                 <table border="1" width="100%">
                     <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Descrição</th>
-                        <th>Peso</th>
-                        <th>Unidade ID</th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                    </tr>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Descrição</th>
+                            <th>Peso</th>
+                            <th>Unidade ID</th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
                     </thead>
                     <tbody>
-                    @foreach($produtos as $produto)
-                        <tr>
-                            <td>{{$produto->nome}}</td>
-                            <td>{{$produto->descricao}}</td>
-                            <td>{{$produto->peso}}</td>
-                            <td>{{$produto->unidade_id}}</td>
-                            <td><a href="{{route('produto.show', ['produto'=>$produto->id])}}">Visualizar</a></td>
-                            <td><a href="">Excluir</a></td>
-                            <td><a href="{{route('produto.edit', ['produto'=>$produto->id])}}">Editar</a></td>
-                        </tr>
-                    @endforeach
+                        @foreach ($produtos as $produto)
+                            <tr>
+                                <td>{{ $produto->nome }}</td>
+                                <td>{{ $produto->descricao }}</td>
+                                <td>{{ $produto->peso }}</td>
+                                <td>{{ $produto->unidade_id }}</td>
+                                <td><a href="{{ route('produto.show', ['produto' => $produto->id]) }}">Visualizar</a></td>
+                                <td>
+                                    <form id="form_{{ $produto->id }}"
+                                        action="{{ route('produto.destroy', ['produto' => $produto->id]) }}"
+                                        method="post">
+                                        @method('DELETE')
+                                        @csrf
+                                        <!--<button type="submit">Excluir</button>-->
+                                        <a href="#"
+                                            onclick="document.getElementById('form_{{ $produto->id }}').submit()">Excluir</a>
+                                    </form>
+                                </td>
+                                <td><a href="{{ route('produto.edit', ['produto' => $produto->id]) }}">Editar</a></td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
                 <br>
-                {{$produtos->appends($request)->links()}}
-                Exibindo {{$produtos->count()}} fornecedores de {{$produtos->total()}}
-                (de {{$produtos->firstItem()}} a {{$produtos->lastItem()}})
+                {{ $produtos->appends($request)->links() }}
+                Exibindo {{ $produtos->count() }} fornecedores de {{ $produtos->total() }}
+                (de {{ $produtos->firstItem() }} a {{ $produtos->lastItem() }})
             </div>
         </div>
 
