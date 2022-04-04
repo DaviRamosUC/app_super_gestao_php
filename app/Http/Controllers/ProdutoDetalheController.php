9610a58fb92713a978209dfc460282e344b3bac6
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-04-03 21:21:21 -0300

    Eloquent ORM 1 para 1 Implement produto detalhes

--- app/Http/Controllers/ProdutoDetalheController.php
+++ app/Http/Controllers/ProdutoDetalheController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Unidade;
+use App\ProdutoDetalhe;
 
 class ProdutoDetalheController extends Controller
 {
@@ -13,7 +15,7 @@ class ProdutoDetalheController extends Controller
      */
     public function index()
     {
-        //
+        //return view('app.produt_detalhe')
     }
 
     /**
@@ -23,7 +25,8 @@ class ProdutoDetalheController extends Controller
      */
     public function create()
     {
-        //
+        $unidades = Unidade::all();
+        return view('app.produto_detalhe.create', ['unidades' => $unidades]);
     }
 
     /**
@@ -34,7 +37,8 @@ class ProdutoDetalheController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        ProdutoDetalhe::create($request->all());
+        echo 'Cadastro realizado com sucesso';
     }
 
     /**
